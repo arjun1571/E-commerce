ba2f6e7e9e4c6f186cfcbba03b6514964f3d0a83
Arjun Das <arjundasback515@gmail.com>
2023-02-03 11:13:55 +0530

    cart price set on cart component

--- src/component/Header/Cart/Cart.js
+++ src/component/Header/Cart/Cart.js
@@ -1,10 +1,24 @@
 import React from 'react';
 
 const Cart = ({cart}) => {
+
+    console.log(cart);
+    let total =0;
+    let shipping=0;
+    for(const product of cart){
+        total=total + product.price;
+        shipping=shipping + product.shipping;
+        
+    }
+    const tax =(total * 0.1).toFixed(2)
     return (
         <div>
             <h3>Order summary</h3>
-                <p>selected item:{cart.length}</p>    
+                <p>selected item:{cart.length}</p>
+                <p>Total Price:${total}</p>    
+                <p>Total Shipping:${shipping} </p>
+                <p>Tax:${tax}</p>
+                <h2>Grand Total: </h2>
         </div>
     );
 };
