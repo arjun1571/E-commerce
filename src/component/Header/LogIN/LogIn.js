d9718bcddc3e91f0eaea9b740b02f192db244bc1
Arjun Das <arjundasback515@gmail.com>
2023-02-25 23:00:10 +0530

    login form design done

--- src/component/Header/LogIN/LogIn.js
+++ src/component/Header/LogIN/LogIn.js
@@ -1,9 +1,25 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import "./Login.css"
 
 const LogIn = () => {
     return (
-        <div>
-            <h1>thsi is a login page and please 1st login the site </h1>
+        <div className='form-container'> 
+            <h2 className='log-text'>LogIn</h2>
+            <form >
+                <div className='form-input'>
+                    <label htmlFor="eamil">Email</label>
+                    <input type="text" name='email' placeholder='email' required />
+                </div>
+                <div className='form-input'>
+                    <label htmlFor="password">Password</label>
+                    <input type="password" name='password' placeholder='password' required />
+                </div>
+                <button className='button-log'>Log IN</button>
+                <h3 className='logo'>Login</h3>
+                <p>New to Ema-john? <Link to="/signup">Create New Accout</Link> </p>
+                <button className='button-log'>Continue With Google</button>
+            </form>
         </div>
     );
 };
